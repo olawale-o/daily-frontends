ccb86154be3863da1eda7701b30be3254bfeb84e
olawale-o <omoogunolawale@gmail.com>
2022-09-29 11:43:01 +0100

    Refactor code

--- calendar/js/calendar.js
+++ calendar/js/calendar.js
@@ -19,6 +19,27 @@ const MONTHS = [
   "December",
 ];
 const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const showDays = (lastDayIndexOfPreviousMonth, lastDateOfCurrentMonth, next_fourteen_days, lastDateOfPreviousMonth) => {
+  const days = [];
+  for (let x = lastDayIndexOfPreviousMonth; x >= 0; x--) {
+    days.push(`<button type="button" class="day prev-date" disabled>${lastDateOfPreviousMonth - x}</button>`);
+  }
+
+  for (let i = 1; i <= lastDateOfCurrentMonth; i++) {
+    if (i === date.getDate()  && currentMonth === date.getMonth()) {
+      days.push(`<button type="button" class="day today">${i}</button>`);
+    } else {
+      days.push(`<button type="button" class="day">${i}</button>`);
+    }
+  }
+
+  for (let j = 1; j <= next_fourteen_days; j++) {
+    days.push(`<button type="button" class="day next-date" disabled>${j}</button>`);
+  }
+
+  return days;
+};
+
 const renderCalendar = () => {
   const currentMonthDetails = new Date(date.getFullYear(), date.getMonth() + 1, 0);
   const previousMonthDetails = new Date(date.getFullYear(), date.getMonth(), 0);
@@ -35,24 +56,30 @@ const renderCalendar = () => {
   const dateString = document.querySelector(".month__full");
   dateString.innerHTML = date.toDateString();
 
-  let days = "";
+  const days = showDays(lastDayIndexOfPreviousMonth, lastDateOfCurrentMonth, NEXT_FOURTEEN_DAYS, lastDateOfPreviousMonth)
 
-  for (let x = lastDayIndexOfPreviousMonth; x >= 0; x--) {
-    days += `<button type="button" class="day prev-date" disabled>${lastDateOfPreviousMonth - x}</button>`;
-  }
+  days.forEach((day) => {
+    monthDays.innerHTML += day;
+  });
 
-  for (let i = 1; i <= lastDateOfCurrentMonth; i++) {
-    if (i === date.getDate()  && currentMonth === date.getMonth()) {
-      days += `<button type="button" class="day today">${i}</button>`;
-    } else {
-      days += `<button type="button" class="day">${i}</button>`;
-    }
-  }
+  // let days = "";
 
-  for (let j = 1; j <= NEXT_FOURTEEN_DAYS; j++) {
-    days += `<button type="button" class="day next-date" disabled>${j}</button>`;
-    monthDays.innerHTML = days;
-  }
+  // for (let x = lastDayIndexOfPreviousMonth; x >= 0; x--) {
+  //   days += `<button type="button" class="day prev-date" disabled>${lastDateOfPreviousMonth - x}</button>`;
+  // }
+
+  // for (let i = 1; i <= lastDateOfCurrentMonth; i++) {
+  //   if (i === date.getDate()  && currentMonth === date.getMonth()) {
+  //     days += `<button type="button" class="day today">${i}</button>`;
+  //   } else {
+  //     days += `<button type="button" class="day">${i}</button>`;
+  //   }
+  // }
+
+  // for (let j = 1; j <= NEXT_FOURTEEN_DAYS; j++) {
+  //   days += `<button type="button" class="day next-date" disabled>${j}</button>`;
+  //   monthDays.innerHTML = days;
+  // }
 };
 
 prevDate.addEventListener("click", () => {
