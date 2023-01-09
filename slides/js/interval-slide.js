34d50c33cfbef06e15ea69b6783978ae84a5827a
olawale-o <omoogunolawale@gmail.com>
2023-01-10 00:44:33 +0100

    Create interval slide

---
+++ slides/js/interval-slide.js
@@ -0,0 +1,24 @@
+var i = 0;
+var images = [];
+var time = 2000;
+
+images[0] = 'https://i.postimg.cc/d0fgZ9k2/image1.jpg';
+images[1] = 'https://i.postimg.cc/t4TrdxH2/image2.jpg';
+images[2] = 'https://i.postimg.cc/GtXF9X9r/image3.jpg';
+images[3] = 'https://i.postimg.cc/cHb1HYZ2/image4.jpg';
+
+const rows = document.querySelectorAll('.row');
+
+function changeImg(){
+  document.slide.src = images[i];
+  rows.forEach(row => row.classList.remove('active'));
+  if(i < images.length - 1){
+    i++;
+  } else {
+    i = 0
+  }
+  rows[i].classList.add('active');
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
